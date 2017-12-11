198006483784ec914510f057dcb9b3910f31fc1f
Andy Bold <andy@120technology.co.uk>
2017-12-11 10:36:19 +0000

    Add: Minor config tweaks

--- custom-settings.el
+++ custom-settings.el
@@ -4,64 +4,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#292929" "#CC555A" "#98A84B" "#D0B344" "#60bAB1" "#FD5FF0" "#68A0B3" "#C5C8C6"])
  '(company-ghc-show-info t)
- '(compilation-message-face (quote default))
  '(epg-gpg-program "/usr/local/MacGPG2/bin/gpg2")
- '(fci-rule-color "#333333")
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
  '(haskell-process-suggest-remove-import-lines t)
  '(haskell-process-type (quote cabal-repl))
  '(haskell-tags-on-save t)
- '(highlight-changes-colors (quote ("#FD5FF0" "#FF2C7A")))
- '(highlight-tail-colors
-   (quote
-    (("#333333" . 0)
-     ("#679A01" . 20)
-     ("#4BBEAE" . 30)
-     ("#1DB4D0" . 50)
-     ("#9A8F21" . 60)
-     ("#A75B00" . 70)
-     ("#F309DF" . 85)
-     ("#333333" . 100))))
- '(magit-diff-use-overlays nil)
+ '(org-agenda-files (quote ("~/org/personal.org.gpg")))
  '(package-selected-packages
    (quote
-    (ghc-mode zenburn-theme hyperbole which-key eyebrowse shm ghc-mod company-ghc hindent haskell-mode apache-mode interleave json-navigator urlenc url-util org-bibtex ivy-bibtex vagrant-tramp vagrant dockerfile-mode org-brain docker persp-projectile perspective blog-admin flyspell-correct-ivy flyspell-popup all-the-icons-ivy org-bullets web-mode undo-tree org-pdfview pdf-tools password-store git-timemachine gitattributes-mode gitignore-mode gitconfig-mode git-gutter-fringe elpy org2blog paredit-menu elfeed-org elfeed-goodies elfeed json-mode ox-pandoc neotree spaceline-all-the-icons all-the-icons-dired all-the-icons persp-mode python-mode magit-ediff counsel-projectile counsel-bbdb counsel calfw jira-markup-mode ox-jira org-jira xml-rpc jira ob-sql-mode ob-restclient ob-applescript applescript-mode grab-mac-link osx-lib osx-clipboard osx-browse markdown-toc markdown-mode+ markdown-mode markdown yaml-mode terraform-mode hcl-mode puppet-mode plantuml-mode graphviz-dot-mode nginx-mode pyvenv starter-kit-ruby php+-mode cmake-font-lock cmake-mode ansible-vault ansible company-web company-shell company-restclient company-php company-lua company-jedi company-cmake company-ansible company artbollocks-mode bbdb-ext bbdb paradox achievements mu4e dired+ outline-magic git-auto-commit-mode better-defaults monokai-theme org-plus-contrib use-package-chords)))
- '(paradox-automatically-star t)
- '(pos-tip-background-color "#98A84B")
- '(pos-tip-foreground-color "#292929")
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#CC555A")
-     (40 . "#CF4F1F")
-     (60 . "#C26C0F")
-     (80 . "#D0B344")
-     (100 . "#AB8C00")
-     (120 . "#A18F00")
-     (140 . "#989200")
-     (160 . "#8E9500")
-     (180 . "#98A84B")
-     (200 . "#729A1E")
-     (220 . "#609C3C")
-     (240 . "#4E9D5B")
-     (260 . "#3C9F79")
-     (280 . "#68A0B3")
-     (300 . "#299BA6")
-     (320 . "#2896B5")
-     (340 . "#2790C3")
-     (360 . "#60bAB1"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (unspecified "#292929" "#333333" "#F70057" "#CC555A" "#86C30D" "#98A84B" "#BEB244" "#D0B344" "#40CAE4" "#60bAB1" "#FB35EA" "#FD5FF0" "#74DBCD" "#68A0B3" "#C5C8C6" "#CC555A")))
+    (ranger yaml-mode xml-rpc winum which-key web-mode vagrant-tramp vagrant use-package urlenc undo-tree terraform-mode spaceline-all-the-icons shm rainbow-delimiters puppet-mode password-store paredit-menu paradox ox-pandoc outline-magic osx-lib osx-clipboard osx-browse org-plus-contrib org-pdfview org-jira org-bullets ob-sql-mode ob-restclient ob-redis ob-php ob-mongo ob-applescript nginx-mode neotree markdown-toc markdown-mode+ magit-lfs json-navigator jira-markup-mode jira ivy-bibtex interleave hyperbole hindent groovy-mode graphviz-dot-mode grab-mac-link gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-gutter-fringe git-auto-commit-mode flyspell-popup flyspell-correct-ivy flycheck-plantuml eyebrowse exec-path-from-shell elpy doom-themes dockerfile-mode docker dired+ counsel-projectile counsel-bbdb company-web company-shell company-restclient company-php company-lua company-ghc company-cmake company-ansible cmake-font-lock calfw blog-admin better-defaults auto-compile applescript-mode apache-mode ansible-vault ansible all-the-icons-ivy all-the-icons-dired))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elfeed-search-feed-face ((t (:foreground "windowFrameColor"))))
- '(org-block ((t (:background "gray26" :foreground "#CC555A"))))
- '(variable-pitch ((t (:family "Cooper Hewitt Book")))))
+ )
