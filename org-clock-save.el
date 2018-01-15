1f9e843af5f10a1ff7b401da72f460e4ef7d79f5
Andy Bold <a.bold@kainos.com>
2018-01-15 16:58:52 +0000

    Add: Jira integration

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,1 +1,2 @@
-;; org-clock-save.el - Titan at <2018-01-01 Mon 11:17>
+;; org-clock-save.el - apple.local at <2018-01-12 Fri 16:35>
+(setq org-clock-stored-history '(("/Users/andrewbo/org/clients/kainos/kainos.org.gpg" . 439)))
